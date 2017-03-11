4cc884f86192905add81cb645905094a963b1dd3
Jose Eduardo <pepe.96.jose@gmail.com>
2017-03-11 15:58:55 -0500

    Long click prompts for topic deletion

--- app/src/main/java/com/jeeps/charades/adapters/SavedItemsAdapter.java
+++ app/src/main/java/com/jeeps/charades/adapters/SavedItemsAdapter.java
@@ -1,9 +1,11 @@
 package com.jeeps.charades.adapters;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import com.jeeps.charades.R;
 import com.jeeps.charades.SetupGame;
+import com.jeeps.charades.database.TopicDataSource;
 import com.jeeps.charades.model.Phrase;
 import com.jeeps.charades.model.Topic;
 import com.jeeps.charades.views.CustomButton;
@@ -27,11 +30,13 @@ public class SavedItemsAdapter extends BaseAdapter {
     private Context context;
     private Topic[] items;
     LayoutInflater inflater;
+    TopicDataSource mTopicDataSource;
 
     public SavedItemsAdapter(Context context, Topic[] items) {
         this.context = context;
         this.items = items;
         inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mTopicDataSource = new TopicDataSource(context);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -54,8 +59,43 @@ public class SavedItemsAdapter extends BaseAdapter {
                 loadTopic(currentTopic);
             }
         });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                promptForDeletion(currentTopic);
+                return true;
+            }
+        });
 
         return convertView;
+    }
+
+    private void promptForDeletion(final Topic currentTopic) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Delete Topic?");
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mTopicDataSource.delete(currentTopic.getId());
+
+                        //Update topics
+                        List<Topic> loadedTopics = mTopicDataSource.read();
+                        Topic[] topics = new Topic[loadedTopics.size()];
+                        topics = loadedTopics.toArray(topics);
+                        items = topics;
+
+                        notifyDataSetChanged();
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
     private void loadTopic(Topic currentTopic) {
